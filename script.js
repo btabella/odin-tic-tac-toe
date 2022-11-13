83728d75c07d9a079aa8f9394f801505089f593f
btabella <brylleabella12@gmail.com>
2022-11-13 10:45:53 +0800

    Loop game 5 times

--- script.js
+++ script.js
@@ -10,22 +10,30 @@ function getComputerChoice() {
     };
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie! You both picked ${playerSelection}`;
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rocks beats Scissors";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats Rock";
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beat Paper";
-    } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+
+for (let i = 0; i < 5; i++) {
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return `It's a tie! You both picked ${playerSelection}`;
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            return "You win! Rocks beats Scissors";
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            return "You win! Paper beats Rock";
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            return "You win! Scissors beat Paper";
+        } else {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
     }
+    
+    const playerSelection = prompt("Welcome! Start the game by either picking rock, paper, or scissors").toLowerCase();
+    
+    const computerSelection = getComputerChoice();
+    
+    alert(playRound(playerSelection, computerSelection));
+
 }
 
-const playerSelection = prompt("Welcome! Start the game by either picking rock, paper, or scissors").toLowerCase();
+// tackle the "keeps score and reports a winner or loser at the end"
 
-const computerSelection = getComputerChoice();
-
-alert(playRound(playerSelection, computerSelection));
