221780138ac7a1431c89530aa6f050a1e5e4dab6
btabella <brylleabella12@gmail.com>
2022-11-17 15:19:57 +0800

    Add grammar fixes

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ for (let i = 0; i < 5; i++) {
         if (playerSelection === computerSelection) {
             return `It's a tie! You both picked ${playerSelection}`;
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            return "You win! Rocks beats Scissors";
+            return "You win! Rock beats Scissors";
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             return "You win! Paper beats Rock";
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
@@ -29,13 +29,13 @@ for (let i = 0; i < 5; i++) {
         }
     }
     
-    const playerSelection = prompt("Welcome! Start the game by either picking rock, paper, or scissors").toLowerCase();
+    const playerSelection = prompt("Welcome! Start the game by either picking Rock, Paper, or Scissors").toLowerCase();
     
     const computerSelection = getComputerChoice();
     
     alert(playRound(playerSelection, computerSelection));
 
-    if (playRound(playerSelection, computerSelection) == "You win! Rocks beats Scissors" || playRound(playerSelection, computerSelection) == "You win! Paper beats Rock" || playRound(playerSelection, computerSelection) == "You win! Scissors beat Paper") {
+    if (playRound(playerSelection, computerSelection) == "You win! Rock beats Scissors" || playRound(playerSelection, computerSelection) == "You win! Paper beats Rock" || playRound(playerSelection, computerSelection) == "You win! Scissors beat Paper") {
         playerScore++;
     } else {
         computerScore++;
